63759b6cd4a5d643aaf213f233333b5952cbbdf8
yeefun <szyeefun@gmail.com>
2021-03-05 17:59:16 +0800

    test: build the demo to test the plugin

--- dev/serve.js
+++ dev/serve.js
@@ -1,7 +1,13 @@
 import Vue from 'vue';
+import LazyRenderer from '@/entry.js';
 import Dev from './serve.vue';
 
 Vue.config.productionTip = false;
+
+Vue.use(LazyRenderer, {
+  tagName: 'ul',
+  preLoad: 0.5,
+});
 
 new Vue({
   render: (h) => h(Dev),
--- dev/serve.vue
+++ dev/serve.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
-    <lazy-renderer />
+    <div class="stuff" />
+    <lazy-renderer tagName="span" :preLoad="1">
+      <div class="target" />
+    </lazy-renderer>
+
+    <div class="stuff" />
+    <lazy-renderer>
+      <div class="target" />
+    </lazy-renderer>
+    <div class="stuff" />
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
-import LazyRenderer from '@/lazy-renderer.vue';
+// import LazyRenderer from '@/lazy-renderer.vue';
 
 export default Vue.extend({
   name: 'ServeDev',
-  components: {
-    LazyRenderer,
-  },
+  // components: {
+  //   LazyRenderer,
+  // },
 });
 </script>
+
+<style scoped>
+.stuff {
+  width: 100px;
+  height: 200vh;
+  background-color: green;
+}
+
+.target {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+</style>
